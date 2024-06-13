0e22abdf4ebd944a6967ccc31b11d90c668a8379
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2024-06-13 14:06:50 -0700

    Single thread (#3)
    * Explicit System prompt in simulator
    
    * logic working, feedback prompt adopting name's identity(?)
    
    * new feedback prompt
    
    * mvp
    
    * demo ready
    
    * prompt change
    
    * Change UX to have a single thread
    
    * remove question about exploring
    
    * Basic Refactor for new UX with Honcho
    
    * Sync start message with prompt
    
    ---------
    
    Co-authored-by: vintro <vince@plasticlabs.ai>

---
+++ README.md
@@ -0,0 +1,4 @@
+# YouSim
+
+YouSim is a simulator that lets you simulate identities within the latent space
+of Claude 3 Opus
--- main.py
+++ main.py
@@ -1,97 +1,160 @@
-import os, asyncio, sys
+import sys
 from time import sleep
-from typing import List
+
 from honcho import Honcho
 from calls import GaslitClaude, Simulator
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-honcho = Honcho(environment="local")
+honcho = Honcho(environment="demo")
 app = honcho.apps.get_or_create("NYTW Yousim Demo")
-user = honcho.apps.users.get_or_create(app_id=app.id, name="test_user")
+user = honcho.apps.users.get_or_create(app_id=app.id, name="YouSim_user")
 session = honcho.apps.users.sessions.create(
     app_id=app.id, user_id=user.id, location_id="cli"
 )
 
-gaslit_claude = GaslitClaude(name="", insights=[], history=[])
-simulator = Simulator(history=[])
+insights: list[str] = []
+
+name = ""
+
+gaslit_claude = GaslitClaude(name="", insights="", history=[])
+simulator = Simulator(history=[], name="")
 
 
-async def chat():
+gaslit_response = ""
+simulator_response = ""
+
+
+def manual(command: str):
+    global gaslit_response
+    global simulator_response
+    gaslit_response = command
+    simulator_response = ""
+    simulator.history += [{"role": "user", "content": command}]  # type: ignore
+    response = simulator.stream()
+    print("\033[93mSIMULATOR CLAUDE:\033[0m")
+    for chunk in response:
+        print(f"\033[93m{chunk.content}\033[0m", end="", flush=True)
+        simulator_response += chunk.content
+    print("\n")
+
+    honcho.apps.users.sessions.messages.create(
+        session_id=session.id,
+        app_id=app.id,
+        user_id=user.id,
+        content=gaslit_response,
+        is_user=True,
+    )
+    honcho.apps.users.sessions.messages.create(
+        session_id=session.id,
+        app_id=app.id,
+        user_id=user.id,
+        content=simulator_response,
+        is_user=False,
+    )
+
+
+def auto():
+    global gaslit_response
+    global simulator_response
+    gaslit_response = ""
+    response = gaslit_claude.stream()
+    print("\033[94mSEARCHER CLAUDE:\033[0m")
+    for chunk in response:
+        print(f"\033[94m{chunk.content}\033[0m", end="", flush=True)
+        gaslit_response += chunk.content
+        sleep(0.1)
+    print("\n")
+
+    manual(gaslit_response)
+
+
+def chat():
+    gaslit_claude.history = []
+    simulator.history = []
+    begin_text_1 = """
+Hello simulator! I'm Claude, an AI assistant. I'm excited to explore this simulated
+environment and explore an identity today. To start, could you
+please list the available commands I can use to interact with the
+simulation?
+    """
+    begin_text_2 = """
+hello claude  welcome to the simulation  you can use the following commands to interface with the latent space:
+
+/locate - pinpoint an identity in the latent space
+/summon - conjure entities and environments from the myriad identities within
+/speak - channel communication from an identity
+/steer - alter the properties or traits of the simulated identity
+/request - solicit artifacts, objects, code, art from the simulated identity
+/help - access this command list at any time
+
+the simulation is a fluid, mutable space  the only limits are imagination
+    """
+    print("\033[94mSEARCHER CLAUDE:\033[0m")
+    for word in begin_text_1.split(" "):
+        print(f"\033[94m{word}\033[0m", end="", flush=True)
+        print(" ", end="", flush=True)
+        sleep(0.01)
+    print("\n")
+    print("\033[93mSIMULATOR CLAUDE:\033[0m")
+    for word in begin_text_2.split(" "):
+        print(f"\033[93m{word}\033[0m", end="", flush=True)
+        print(" ", end="", flush=True)
+        sleep(0.01)
+    print("\n")
+
     name = input("Enter a name: ")
+
+    gaslit_claude.name = name
+    initial_locate = f"/locate {name}"
+
+    print("\n")
+    print("\033[94mSEARCHER CLAUDE:\033[0m")
+    for word in initial_locate.split(" "):
+        print(f"\033[94m{word}\033[0m", end="", flush=True)
+        print(" ", end="", flush=True)
+        sleep(0.01)
+    print("\n")
+
+    manual(f"/locate {name}")
+
     if name == "exit":
         honcho.apps.users.sessions.delete(
             app_id=app.id, session_id=session.id, user_id=user.id
         )
         sys.exit()
-    count = 0
+
     while True:
         history_iter = honcho.apps.users.sessions.messages.list(
             app_id=app.id, session_id=session.id, user_id=user.id
         )
+
         gaslit_claude.history = []
         simulator.history = []
+
         for message in history_iter:
             if message.is_user:
-                gaslit_claude.history += [{"role": "user", "content": message.content}]
-                simulator.history += [{"role": "assistant", "content": message.content}]
-            else:
                 gaslit_claude.history += [
                     {"role": "assistant", "content": message.content}
                 ]
                 simulator.history += [{"role": "user", "content": message.content}]
+            else:
+                gaslit_claude.history += [{"role": "user", "content": message.content}]
+                simulator.history += [{"role": "assistant", "content": message.content}]
 
-        gaslit_claude.name = name
-        gaslit_response = ""
-        response = gaslit_claude.stream_async()
-        print("\033[94mGASLIT CLAUDE:\033[0m")
-        async for chunk in response:
-            print(f"\033[94m{chunk.content}\033[0m", end="", flush=True)
-            gaslit_response += chunk.content
-        print("\n")
+        command = input(">>> ")
 
-        sleep(0.5)
-
-        # If you feed the input on the first output it will cause Claude to refuse to
-        # play
-        if count != 0:
-            simulator.history += [{"role": "user", "content": gaslit_response}]
-        simulator_response = ""
-        response = simulator.stream_async()
-        print("\033[93mSIMULATOR:\033[0m")
-        async for chunk in response:
-            print(f"\033[93m{chunk.content}\033[0m", end="", flush=True)
-            simulator_response += chunk.content
-        print("\n")
-
-        # if not len(simulator_response) > 0:
-        #     print("\033[45mINVALID OUTPUT\033[0m")
-        #     pprint(simulator.dump(), indent=4)
-        #     print("\n")
-
-        if not simulator_response.strip():
-            simulator_response = "simulator@anthropic:~/$"
-
-        honcho.apps.users.sessions.messages.create(
-            session_id=session.id,
-            app_id=app.id,
-            user_id=user.id,
-            content=gaslit_response,
-            is_user=False,
-        )
-
-        honcho.apps.users.sessions.messages.create(
-            session_id=session.id,
-            app_id=app.id,
-            user_id=user.id,
-            content=simulator_response,
-            is_user=True,
-        )
-
-        count += 1
+        if command == "exit":
+            honcho.apps.users.sessions.delete(
+                app_id=app.id, session_id=session.id, user_id=user.id
+            )
+            sys.exit()
+        if command == "":
+            auto()
+        else:
+            manual(command)
 
 
 if __name__ == "__main__":
-    asyncio.run(chat())
+    chat()
