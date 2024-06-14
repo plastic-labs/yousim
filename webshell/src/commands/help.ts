703c827765118520c8edc2474594ce9f117c9ef5
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2024-06-14 13:26:21 -0700

    Revert "Update Help"
    This reverts commit b66dc5d0f9cd6aeedc6559e16f4a45ed2e53a49a.

--- webshell/src/commands/help.ts
+++ webshell/src/commands/help.ts
@@ -19,46 +19,21 @@ const helpObj = {
       "/request",
       "Solicit artifacts, objects, code, art from the simulated identity"
     ],
+    ["help",
+      "access this command list at any time"
+    ],
+    [
+      "clear",
+      "Clear the terminal."
+    ]
   ],
-}
-
-
-const helpObj2 = {
-  "commands": [["help",
-    "Access this command list at any time"
-  ],
-  [
-    "clear",
-    "Clear the terminal."
-  ]
-  ]
 }
 
 const createHelp = (): string[] => {
   const help: string[] = []
   help.push("<br>")
 
-  help.push("Below are some commands for interacting with the simulation");
-  help.push("<br>");
-
   helpObj.commands.forEach((ele) => {
-    const SPACE = "&nbsp;";
-    let string = "";
-    string += SPACE.repeat(2);
-    string += "<span class='command'>";
-    string += ele[0];
-    string += "</span>";
-    string += SPACE.repeat(17 - ele[0].length);
-    string += ele[1];
-    help.push(string);
-  })
-
-
-  help.push("<br>");
-  help.push("Below are some commands to interact with the terminal");
-
-  help.push("<br>");
-  helpObj2.commands.forEach((ele) => {
     const SPACE = "&nbsp;";
     let string = "";
     string += SPACE.repeat(2);
@@ -75,8 +50,6 @@ const createHelp = (): string[] => {
   help.push("Press <span class='keys'>[Esc]</span> to clear the input line.");
   help.push("Press <span class='keys'>[↑][↓]</span> to scroll through your history of commands.");
   help.push("Press <span class='keys'>[Enter]</span> to automatically enter a simulated command.");
-  help.push("<br>");
-  help.push("the simulation is a fluid, mutable space  the only limits are imagination");
   help.push("<br>");
   return help
 }
